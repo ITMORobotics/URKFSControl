da6eb61d8b3e48a3be3cbbc5b870f89c42a79eb4
TexnoMan <antipov.v.a@yandex.ru>
2022-01-22 22:57:54 +0300

    Update control for cooperative system

--- coop/coop_robot.py
+++ coop/coop_robot.py
@@ -19,7 +19,9 @@ from kdl_parser.kdl_parser_py.kdl_parser_py import urdf
 
 
 class DualUniversalRobot(UniversalRobot):
-    def __init__(self, ip1: str, ip2: str):
+    def __init__(self, ip1: str, ip2: str, dt: float):
+        self.__acceleration = 0.5
+        self.__dt = dt
         self.__robots = (UniversalRobot(ip1), UniversalRobot(ip2) )
         self.__state = (RobotState(), RobotState())
         self.update_state()
@@ -37,7 +39,23 @@ class DualUniversalRobot(UniversalRobot):
         return self.__state
 
     def update_state(self):
+        self.__robots[0].update_state()
+        self.__robots[1].update_state()
         self.__state = (self.__robots[0].state, self.__robots[1].state)
+    
+    def send_dq(self, dq_common):
+        self.__robots[0].control.speedJ(dq_common[0:6], self.__acceleration, self.__dt); 
+        self.__robots[1].control.speedJ(dq_common[6:], self.__acceleration, self.__dt) 
+    
+    def stop(self):
+        self.__robots[0].control.speedStop()
+        self.__robots[1].control.speedStop()
+        self.__robots[0].control.stopScript()
+        self.__robots[1].control.stopScript()
+
+    def __del__(self):
+        self.__robots[0].__del__()
+        self.__robots[1].__del__()
 
 
 class DualCoopModel:
@@ -48,30 +66,34 @@ class DualCoopModel:
             raise(RuntimeError('Current coop model does not support more than two robot models'))
     
     def relative_jacobian(self, q_tuple: Tuple[np.ndarray]) -> np.ndarray:
-        rel_jacob = np.concatenate([self.__coop_model[1].jacobian(q_tuple[1]), -self.__coop_model[0].jacobian(q_tuple[0])], axis=1)
+        rel_jacob = np.concatenate((-self.__coop_model[0].jacobian(q_tuple[0]), self.__coop_model[1].jacobian(q_tuple[1])), axis=1)
         return rel_jacob
     
     def absolute_jacobian(self, q_tuple: Tuple[np.ndarray]) -> np.ndarray:
-        rel_jacob = np.concatenate([1.0/2*self.__coop_model[1].jacobian(q_tuple[1]), 1.0/2*self.__coop_model[0].jacobian(q_tuple[0])], axis=1)
+        rel_jacob = np.concatenate((1.0/2*self.__coop_model[0].jacobian(q_tuple[0]), 1.0/2*self.__coop_model[1].jacobian(q_tuple[1])), axis=1)
         return rel_jacob
+
+    def abs_rel_jacobian(self, q_tuple: Tuple[np.ndarray]) -> np.ndarray:
+        return np.concatenate((self.absolute_jacobian(q_tuple), self.relative_jacobian(q_tuple)), axis=0)
         
     def relative_orient(self, q_tuple:Tuple[np.ndarray]) -> np.ndarray:
         rot_left = self.__coop_model[0].rot(q_tuple[0])
         rot_right = self.__coop_model[1].rot(q_tuple[1])
-        rot_relative = np.transpose(rot_left).dot(rot_right)
+        rot_relative = rot_left*rot_right.T
         return rot_relative
 
     def absolute_orient(self, q_tuple:Tuple[np.ndarray]) -> np.ndarray:
         rot_left = self.__coop_model[0].rot(q_tuple[0])
-        rot_right = self.__coop_model[1].rot(q_tuple[1])
-        rot_relative = np.transpose(rot_left).dot(rot_right)
-        angvec_relative = list(spmb.tr2angvec(rot_relative, unit='rad', check=False))
-        print(angvec_relative)
-        angvec_relative[0] /= 2.0
-        print(angvec_relative)
-        half_rot_relative = spmb.angvec2r(angvec_relative[0], angvec_relative[1])
+        # rot_right = self.__coop_model[1].rot(q_tuple[1])
+        # rot_relative = rot_right @ rot_left.T
+        # angvec_relative = list(spmb.tr2angvec(rot_relative, unit='rad', check=False))
+        # # print(angvec_relative)
+        # angvec_relative[0] /= 2.0
+        # # print(angvec_relative)
+        # half_rot_relative = spmb.angvec2r(angvec_relative[0], angvec_relative[1])
 
-        rot_abs = rot_left.dot(half_rot_relative)
+        # rot_abs = rot_left @ half_rot_relative
+        rot_abs = rot_left 
         return rot_abs
     
     def absolute_pose(self, q_tuple:Tuple[np.ndarray]) -> np.ndarray:
@@ -81,18 +103,24 @@ class DualCoopModel:
         return 1/2.0*(pose_left + pose_right)
     
     def relative_pose(self, q_tuple:Tuple[np.ndarray]) -> np.ndarray:
-        print("---")
+        # print("---")
         pose_left = self.__coop_model[0].pose_angvec(q_tuple[0])[0]
-        print(pose_left)
+        # print(pose_left)
         pose_right = self.__coop_model[1].pose_angvec(q_tuple[1])[0]
-        print(pose_right)
+        # print(pose_right)
         return pose_right - pose_left
     
-    def relative_force(self, ft: Tuple[np.ndarray]):
+    def relative_force_torque(self, ft: Tuple[np.ndarray]) -> np.ndarray:
         return ft[1] - ft[0]
     
-    def absolute_force(self, ft: Tuple[np.ndarray]):
-        return 1/2*(ft[0] + ft[1])
+    def absolute_force_torque(self, ft: Tuple[np.ndarray]) -> np.ndarray:
+        return 1/2.0*(ft[0] + ft[1])
+    
+    def abs_rel_pose(self, q_tuple:Tuple[np.ndarray]) -> np.ndarray:
+        return np.concatenate((self.absolute_pose(q_tuple), self.relative_pose(q_tuple)), axis=0)
+    
+    def abs_rel_force(self, ft:Tuple[np.ndarray]) -> np.ndarray:
+        return np.concatenate((self.absolute_force(ft), self.relative_force(ft)), axis=0)
 
     @property
     def size(self):
--- test/read_data.py
+++ test/read_data.py
@@ -8,19 +8,32 @@ from rtde_receive import RTDEReceiveInterface
 import time
 
 
-robot_res = RTDEReceiveInterface('192.168.88.6')
-robot_control = RTDEControlInterface('192.168.88.6')
+robot_res = RTDEReceiveInterface('192.168.88.5')
+robot_control = RTDEControlInterface('192.168.88.5')
 robot_control.teachMode()
 
+robot_res2 = RTDEReceiveInterface('192.168.88.6')
+robot_control2 = RTDEControlInterface('192.168.88.6')
+robot_control2.teachMode()
+
+dt=0.02
 data = []
 start_time = time.time()
-while (time.time() - start_time) < 20.0:
+
+while (time.time() - start_time) < 100.0:
+    start_loop_time = time.time()
     q = robot_res.getActualQ()
     pose = robot_res.getActualTCPPose()
-    row = np.concatenate([np.array(q),np.array(pose)])
+    q2 = robot_res2.getActualQ()
+    pose2 = robot_res2.getActualTCPPose()
+    row = np.concatenate([np.array(q),np.array(pose), np.array(q2),np.array(pose2)])
     print(row)
     data.append(row)
-    time.sleep(0.1)
 
-data_pd = pd.DataFrame(data, columns=['q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'px', 'py', 'pz', 'rx', 'ry', 'rz'])
-data_pd.to_csv("result.csv")
+    end_loop_time = time.time()
+    duration = end_loop_time - start_loop_time
+    if duration < dt:
+        time.sleep(dt - duration)
+
+data_pd = pd.DataFrame(data, columns=['ql0', 'ql1', 'ql2', 'ql3', 'ql4', 'ql5', 'plx', 'ply', 'plz', 'rlx', 'rly', 'rlz', 'qr0', 'qr1', 'qr2', 'qr3', 'qr4', 'qr5', 'prx', 'pry', 'prz', 'rrx', 'rry', 'rrz'])
+data_pd.to_csv("result2.csv")
