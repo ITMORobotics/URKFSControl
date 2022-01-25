3ee26d7316fa474146c3e6d830cbe7e1921a193a
Nikita Popov <nikita29042001@yandex.ru>
2022-01-25 18:14:27 +0300

    Both grippers control

--- test/gripper_test.py
+++ test/gripper_test.py
@@ -1,7 +1,9 @@
 import serial
 import sys
 import os
-
+import glob
+import logging
+import time
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from gripper_controller import GripperSerialController
 
@@ -22,15 +24,27 @@ def search_serial_ports():
 
 
 def main():
+    logging.basicConfig(level=logging.NOTSET)
+    logger = logging.getLogger("Grippers")
     serial_ports = search_serial_ports()
     grippers_dict = {}
 
     for counter, value in enumerate(serial_ports):
-        grippers_dict[counter] = GripperSerialController(value, 57600)
+        if 'ACM' in value:
+            grippers_dict[counter] = GripperSerialController(value, 57600)
+            time.sleep(0.2)
+    
+    logger.info(" Found %d grippers"%len(grippers_dict))
+    for i in range(20):    
+        grippers_dict[0].close()
+        grippers_dict[1].close()
+        time.sleep(3)
+        grippers_dict[0].open()
+        grippers_dict[1].open()
+        time.sleep(3)
 
-    print(girppers_dict)
-    print("Prekol")
-
+    #for counter, _ in enumerate(grippers_dict):
+        #grippers_dict[counter].close()
 
 if __name__ == "__main__":
     main()
